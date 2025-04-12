28d60bd2efa2c34325d23a1fc74dd56f9e17a635
Hector Rivera <hector@hyrivera.com>
2025-04-11 22:51:24 -0400

    Update socket connection to use dynamic origin instead of hardcoded localhost URL

--- src/socket.ts
+++ src/socket.ts
@@ -12,7 +12,7 @@ export interface RoomState {
   revealed: boolean;
 }
 
-const socket: Socket = io('http://localhost:3001', {
+const socket: Socket = io(window.location.origin, {
   reconnection: true,
   reconnectionAttempts: 5,
   reconnectionDelay: 1000,
